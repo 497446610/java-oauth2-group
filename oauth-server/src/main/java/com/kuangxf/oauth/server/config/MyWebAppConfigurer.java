b0eac0d36b7392b120d8c6c216edff0baa818ef5
PC-20180202FXRO\Administrator <497446610@qq.com>
2018-02-08 15:10:47 +0800

    spring 官方oauth2 demo，待下一步调整

---
+++ oauth-server/src/main/java/com/kuangxf/oauth/server/config/MyWebAppConfigurer.java
@@ -0,0 +1,23 @@
+/*
+ * @(#)MyWebAppConfigurer.java        1.0 2018年2月8日
+ *
+ *
+ */
+
+package com.kuangxf.oauth.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyWebAppConfigurer 
+        extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+
+}
